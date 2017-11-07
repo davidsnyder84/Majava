8d9fca8221dcd65a8b79f9d86a23b8ed3e4c3ccd
Rusty Shackleford <davidsnyder84@gmail.com>
2017-11-07 15:06:22 -0600

    Better identifiers in Game

--- src/majava/Game.java
+++ src/majava/Game.java
@@ -37,16 +37,12 @@ public class Game {
 			}
 		}
 	}
-//	public static final GameType GAME_TYPE_DEFAULT = GameType.SINGLE;
-	public static final GameType GAME_TYPE_DEFAULT = GameType.HANCHAN;
-//	public static final GameType GAME_TYPE_DEFAULT = GameType.TONPUUSEN;
+//	private static final GameType GAME_TYPE_DEFAULT = GameType.SINGLE;
+	private static final GameType GAME_TYPE_DEFAULT = GameType.HANCHAN;
+//	private static final GameType GAME_TYPE_DEFAULT = GameType.TONPUUSEN;
 
 	private static final boolean DEFAULT_DO_FAST_GAMEPLAY = false;
-	private static final boolean DEFAULT_FAST_COMS_MEAN_SIMULATION = true;
-//	private static final boolean DEFAULT_FAST_COMS_MEAN_SIMULATION = false;
 	private static final int NUM_SIMULATIONS_TO_RUN = 500;
-//	private static final int NUM_SIMULATIONS_TO_RUN = 5000;
-//	private static final int NUM_SIMULATIONS_TO_RUN = 100;
 	
 	
 	
@@ -55,19 +51,19 @@ public class Game {
 	private final Player p1, p2, p3, p4;
 	private final Player[] mPlayerArray;
 	
-	private Round mCurrentRound;
-	private Wind mCurrentRoundWind;
-	private int mCurrentRoundNum, mCurrentRoundBonusNum;
+	private Round currentRound;
+	private Wind currentRoundWind;
+	private int currentRoundNum, currentRoundBonusNum;
 	
-	private GameType mGameType;
-	private boolean mGameIsOver;
-	private int mNumRoundsPlayed;
-	private final List<String> mWinStrings;
-	private final List<RoundResultSummary> mRoundResults;
+	private GameType gameType;
+	private boolean gameIsOver;
+	private int numRoundsPlayed;
+	private final List<String> winStrings;
+	private final List<RoundResultSummary> roundResults;
 	
-	private final GameUI mUI;
+	private final GameUI userInterface;
 	
-	private boolean mDoFastGameplay;
+	private boolean doFastGameplay;
 	
 	
 	
@@ -81,24 +77,24 @@ public class Game {
 		p1 = mPlayerArray[0]; p2 = mPlayerArray[1]; p3 = mPlayerArray[2]; p4 = mPlayerArray[3];
 		
 		//initializes round info
-		mCurrentRoundWind = DEFAULT_ROUND_WIND;mCurrentRoundNum = 1;mCurrentRoundBonusNum = 0;
-		mNumRoundsPlayed = 0;
+		currentRoundWind = DEFAULT_ROUND_WIND;currentRoundNum = 1;currentRoundBonusNum = 0;
+		numRoundsPlayed = 0;
 		
-		mGameIsOver = false;
-		mWinStrings = new ArrayList<String>();
-		mRoundResults = new ArrayList<RoundResultSummary>();
+		gameIsOver = false;
+		winStrings = new ArrayList<String>();
+		roundResults = new ArrayList<RoundResultSummary>();
 		
-		mUI = ui;
+		userInterface = ui;
 		__setUIs();
 		
-		mDoFastGameplay = DEFAULT_DO_FAST_GAMEPLAY;
+		doFastGameplay = DEFAULT_DO_FAST_GAMEPLAY;
 	}
 	
 	
 	
 	//set the game type
 	private void setGameType(GameType gametype){
-		mGameType = gametype;
+		gameType = gametype;
 	}
 	public void setGameTypeSingle(){setGameType(GameType.SINGLE);}
 	public void setGameTypeTonpuusen(){setGameType(GameType.TONPUUSEN);}
@@ -115,7 +111,7 @@ public class Game {
 		
 		checkPlayersAndUI();
 		
-		while (!gameIsOver()){
+		while (!gameHasEnded()){
 			playNewRound();
 			saveRoundResults();
 			incrementRound();
@@ -124,50 +120,53 @@ public class Game {
 	}
 	
 	private void playNewRound(){
-		//play a round
-		mCurrentRound = new Round(mUI, mPlayerArray, mCurrentRoundWind, mCurrentRoundNum, mCurrentRoundBonusNum);
-		mCurrentRound.setOptionFastGameplay(mDoFastGameplay);
-		mCurrentRound.play();
+		currentRound = new Round(userInterface, mPlayerArray, currentRoundWind, currentRoundNum, currentRoundBonusNum);
+		currentRound.setOptionFastGameplay(doFastGameplay);
+		currentRound.play();
 	}
 	
 	private void saveRoundResults(){
-		mNumRoundsPlayed++;
-		System.out.println(mNumRoundsPlayed);
-		if (mCurrentRound.endedWithVictory()){
-			mWinStrings.add(mCurrentRound.getWinningHandString());
+		numRoundsPlayed++;
+		System.out.println("End of round" + numRoundsPlayed);
+		if (currentRound.endedWithVictory()){
+			winStrings.add(currentRound.getWinningHandString());
 		}
-		mRoundResults.add(mCurrentRound.getResultSummary());
+		roundResults.add(currentRound.getResultSummary());
 	}
 	
 	private void incrementRound(){
 		//move to the next round, or do a bonus round if the dealer won
-		if (mCurrentRound.qualifiesForRenchan()){
-			mCurrentRoundBonusNum++;
+		if (currentRound.qualifiesForRenchan()){
+			currentRoundBonusNum++;
 		}
 		else{
 			//move to the next round
-			mCurrentRoundNum++;
-			mCurrentRoundBonusNum = 0;
+			currentRoundNum++;
+			currentRoundBonusNum = 0;
 			
 			rotateSeats();
 			
 			//advance to the next wind if all rounds are finished
-			if (mCurrentRoundNum > MAX_NUM_ROUNDS){
-				mCurrentRoundNum = 1;
-				mCurrentRoundWind = mCurrentRoundWind.next();
+			if (currentRoundNum > MAX_NUM_ROUNDS){
+				currentRoundNum = 1;
+				currentRoundWind = currentRoundWind.next();
 			}
 		}
 	}
 	
 	private void checkPlayersAndUI(){
-		if (DEFAULT_FAST_COMS_MEAN_SIMULATION)
-			if (mDoFastGameplay && p1.controllerIsComputer() && p2.controllerIsComputer() && p3.controllerIsComputer() && p4.controllerIsComputer()){
-				setGameTypeSimulation();
-				for (Player p: mPlayerArray) p.setControllerComputer();
-			}
-		if (mUI == null) for (Player p: mPlayerArray) p.setControllerComputer();
+		if (userInterface == null)
+			for (Player p: mPlayerArray) p.setControllerComputer();
+		
+		if (doFastGameplay && allPlayersAreComputers())
+			setGameTypeSimulation();
 	}
 	
+	
+	private boolean allPlayersAreComputers(){
+		for (Player p: mPlayerArray) if (p.controllerIsHuman()) return false;
+		return true;
+	}
 	
 	
 	private void handleEndGame(){
@@ -184,7 +183,7 @@ public class Game {
 	
 	private void __setUIs(){
 		for (Player p: mPlayerArray){
-			p.setUI(mUI);
+			p.setUI(userInterface);
 		}
 	}
 	
@@ -194,12 +193,12 @@ public class Game {
 		System.out.println("\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~\nGAMEOVER\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 		System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~\nPrinting win results\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 		
-		for (int i = 0; i < mWinStrings.size(); i++)
-			System.out.printf("%3d: %s\n", i+1, mWinStrings.get(i));
+		for (int i = 0; i < winStrings.size(); i++)
+			System.out.printf("%3d: %s\n", i+1, winStrings.get(i));
 		
 		RoundResultSummary cr = null;
-		for (int i = 0; i < mRoundResults.size(); i++){
-			cr = mRoundResults.get(i);
+		for (int i = 0; i < roundResults.size(); i++){
+			cr = roundResults.get(i);
 			if (cr != null && !cr.isDrawWashout())
 				System.out.printf("%3d: %s\n", i+1, cr.getAsStringResultType());
 		}
@@ -209,7 +208,7 @@ public class Game {
 		int numWins = 0;
 		int numWinsByPlayer[] = new int[4];
 		
-		for (RoundResultSummary cr: mRoundResults){
+		for (RoundResultSummary cr: roundResults){
 			if (cr != null && cr.isVictory()){
 				numWins++;
 				numWinsByPlayer[cr.getWinningPlayer().getPlayerNumber()]++;
@@ -221,40 +220,46 @@ public class Game {
 		int playerNum = 0;
 		for (int i: numWinsByPlayer)
 			System.out.println("\tPlayer " + (++playerNum) + ": " + i + " wins");
-		System.out.printf("Win to draw ratio: %.6f", ((double) numWins) / ((double) mRoundResults.size()));
+		System.out.printf("Win to draw ratio: %.6f", ((double) numWins) / ((double) roundResults.size()));
 		
 		System.out.println("\n\n");
 	}
 	
 	
 	
-	public boolean gameIsOver(){
-		if (mGameIsOver) return true;
+	public boolean gameHasEnded(){
+		if (gameIsOver) return true;
 		
-		for (Player p: mPlayerArray) if (mGameType != GameType.SIMULATION && p.getPoints() < 0) return mGameIsOver = true;
+		//game is over is someone runs out of points
+		for (Player p: mPlayerArray)
+			if (gameType != GameType.SIMULATION && p.getPoints() <= 0)
+				return gameIsOver = true;
 		
-		switch (mGameType){
-		case SINGLE: return mGameIsOver = (mCurrentRoundNum > 1 || mCurrentRoundBonusNum > 0);
-		case TONPUUSEN: case HANCHAN: return mGameIsOver = (mCurrentRoundWind == mGameType.finalWind().next());
-		case SIMULATION: return mGameIsOver = (mNumRoundsPlayed >= NUM_SIMULATIONS_TO_RUN);
+		switch (gameType){
+		case SINGLE:
+			return gameIsOver = (currentRoundNum > 1 || currentRoundBonusNum > 0);
+		case TONPUUSEN: case HANCHAN:
+			return gameIsOver = (currentRoundWind == gameType.finalWind().next());
+		case SIMULATION:
+			return gameIsOver = (numRoundsPlayed >= NUM_SIMULATIONS_TO_RUN);
 		default: return false;
 		}
 	}
 	
 	
-	public void setOptionFastGameplay(boolean doFastGameplay){mDoFastGameplay = doFastGameplay;}
+	public void setOptionFastGameplay(boolean doFast){doFastGameplay = doFast;}
 	
 	
 	
 	
 	
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Majava (Game)!");
-		
-		System.out.println("Launching Table...");
-		Table.main(null);
-	}
+//	public static void main(String[] args) {
+//		
+//		System.out.println("Welcome to Majava (Game)!");
+//		
+//		System.out.println("Launching Table...");
+//		Table.main(null);
+//	}
 	
 
 }

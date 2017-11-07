3fb8fefc2ee934e8d564dcfee1e89044f0fae8cb
Rusty Shackleford <davidsnyder84@gmail.com>
2017-11-07 10:58:25 -0600

    Changed Fu to calculated attribute
    Changed Fu from member variable to a calculated attribute in Meld

--- src/majava/Meld.java
+++ src/majava/Meld.java
@@ -3,7 +3,6 @@ package majava;
 
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 
 import majava.tiles.GameTile;
 import majava.util.GameTileList;
@@ -11,48 +10,9 @@ import majava.enums.MeldType;
 import majava.enums.Wind;
 
 
-/*
-Class: Meld
-represents a meld of tiles (chi, pon, kan, pair)
 
-data:
-	mTiles - list of tiles in the meld
-	mMeldType - the type of meld (chi, pon, kan, pair)
-	mClosed = if the meld is a closed meld, this will be true, false otherwise
-	mFu - the fu value of the meld
-	
-	mOwnerSeatWind - the seat wind of the owner of the meld (needed for checking fu of wind pairs)
-	
-	mCompletedTile - the tile that completed the meld
-	mPlayerResponsible - the player who contributed the completing tile to the meld
-	
-methods:
-	constructors:
-	3-arg: requires list of tiles from hand, the completing tile, and meld type
-	2-arg: requires list of tiles from hand and meld type
-	copy constructor
-	
-	public:
-		mutators:
-		upgradeToMinkan - uses the given tile to upgrade an open pon to a minkan
-		
-		
-	 	accessors:
-	 	calculateFu - returns the amount of fu points the meld is worth
-	 	isClosed - returns true if the meld is closed
-	 	
-	 	getOwnerSeatWind - returns the seat wind of the meld's owner
-	 	getResponsible - returns the seat wind of the player responsible for completing the meld
-	 	
-	 	getTile - returns the tile at the given index in the meld
-	 	getAllTiles - returns a list of all tiles in the meld
-		getSize - returns how many tiles are in the meld
-		isChi, isPon, isKan - returns true if the meld is of the corresponding type
-*/
+//represents a meld (–ÊŽq) of tiles (chi, pon, kan, pair)
 public class Meld implements Iterable<GameTile>, Comparable<Meld>, Cloneable {
-	
-	private static final int FU_DEFAULT = 0;
-	
 	
 	private GameTileList mTiles;	
 	private MeldType mMeldType;
@@ -61,17 +21,11 @@ public class Meld implements Iterable<GameTile>, Comparable<Meld>, Cloneable {
 	private Wind mOwnerSeatWind;
 	private Wind mPlayerResponsible;
 	
-	private int mFu;
-	
-	
-	
 	
 	
 	public Meld(GameTileList tilesFromHand, GameTile completingTile, MeldType meldType){
 		mTiles = new GameTileList();
 		formMeld(tilesFromHand, completingTile, meldType);
-		
-		mFu = FU_DEFAULT;
 	}
 	//2-arg, takes list of tiles and meld type (used when making a meld only from hand tiles, so no "new" tile)
 	//passes (handtiles 0 to n-1, handtile n, and meld type)
@@ -80,19 +34,15 @@ public class Meld implements Iterable<GameTile>, Comparable<Meld>, Cloneable {
 	}
 	
 	
-	private Meld(Meld other){
-		
+	private Meld(Meld other){		
 		mTiles = other.getAllTiles();
 		
-		mOwnerSeatWind = other.mOwnerSeatWind;
-		
-		mFu = other.mFu;
-		
 		mCompletedTile = other.mCompletedTile;
+		
+		mOwnerSeatWind = other.mOwnerSeatWind;
 		mPlayerResponsible = other.mPlayerResponsible;
 	}
 	public Meld clone(){return new Meld(this);}
-	
 	
 	
 	
@@ -110,8 +60,6 @@ public class Meld implements Iterable<GameTile>, Comparable<Meld>, Cloneable {
 		
 		if (isChi()) sort();
 	}
-	
-	
 	
 	
 	//uses the given tile to upgrade a minkou to a minkan
@@ -133,15 +81,11 @@ public class Meld implements Iterable<GameTile>, Comparable<Meld>, Cloneable {
 	
 	
 	
-	
 	//returns the amount of fu the meld is worth
 	public int calculateFu(){
 		int fu = 0;
-		
 		//////implement fu calculation here
-		
-		mFu = fu;
-		return mFu;
+		return fu;
 	}
 	
 	

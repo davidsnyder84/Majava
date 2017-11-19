2532b573d0cfd6ad3b4608a7fd58fbcaab89ef48
Rusty Shackleford <davidsnyder84@gmail.com>
2017-11-19 12:17:54 -0600

    Made indexOfRiichiTile a calculated field in Pond

--- src/majava/Pond.java
+++ src/majava/Pond.java
@@ -11,45 +11,33 @@ import majava.util.GameTileList;
 //represents a player's pond (êÏ) of discarded tiles
 public class Pond implements Cloneable{
 	private static final int SIZE_MAX = 30;
-	private static final int NO_RIICHI_INDEX = -1;
-	
 	
 	private final List<PondTile> tiles;
-	private int indexOfRiichiTile;
-	
-	
 	
 	public Pond(){
 		tiles = new ArrayList<PondTile>(SIZE_MAX);
-		indexOfRiichiTile = NO_RIICHI_INDEX;
 	}
 	public Pond(Pond other){
 		tiles = new ArrayList<PondTile>(SIZE_MAX);
 		for (PondTile t: other.tiles) tiles.add(t.clone());
-		
-		indexOfRiichiTile = other.indexOfRiichiTile;
 	}
 	public Pond clone(){return new Pond(this);}
 	
 	
-	public void addTile(GameTile t){tiles.add(new PondTile(t));}
 	
-	public PondTile getRiichiTile(){return getTile(indexOfRiichiTile);}
+	//accessors
+	public int indexOfRiichiTile(){
+		for (int index = 0; index < size(); index++)
+			if (getTile(index).isRiichiTile())
+				return index;		
+		return -1;
+	}
+	public PondTile getRiichiTile(){return getTile(indexOfRiichiTile());}
 	public PondTile getMostRecentTile(){return getTile(size() - 1);}
 	public int size(){return tiles.size();}
 	
 	public PondTile getTile(int index){return tiles.get(index);}
 	public GameTileList getTilesAsList(){return new GameTileList(tiles);}
-	
-	
-	//marks the most recent tile as missing (because it was callled)
-	public PondTile removeMostRecentTile(){
-		getMostRecentTile().setCalled();
-		return getMostRecentTile();
-	}
-	
-	
-	
 	
 	//nagashi mangan = composed of only TYC, and no tiles have been called
 	public boolean isElligibleForNagashiMangan(){
@@ -57,6 +45,16 @@ public class Pond implements Cloneable{
 			if (!t.isYaochuu() || t.wasCalled())
 				return false;
 		return true;
+	}
+	
+	
+	//mutators
+	public void addTile(GameTile t){tiles.add(new PondTile(t));}
+	
+	//the removed tile will remain in the pond, but it will be marked as "called"
+	public PondTile removeMostRecentTile(){
+		getMostRecentTile().setCalled();
+		return getMostRecentTile();
 	}
 	
 	
